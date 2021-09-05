e4138fb1e33000e72d2d71373de4ab9926c90d66
Charlene <you@example.com>
2021-09-06 00:42:39 +0800

    edit

--- src/main/java/com/collab/g5/demo/regulations/RegulationLimitRepository.java
+++ src/main/java/com/collab/g5/demo/regulations/RegulationLimitRepository.java
@@ -3,5 +3,5 @@ package com.collab.g5.demo.regulations;
 import com.collab.g5.demo.regulations.RegulationLimit;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RegulationLimitRepository extends JpaRepository<RegulationLimit, String> {
+public interface RegulationLimitRepository extends JpaRepository<RegulationLimit, RegulationLimitKey> {
 }
