0ec4ee6bca3595f3f1d593ea12dc6ff758838bff
Eaint <eaintthirikhit@gmail.com>
2021-09-08 17:51:01 +0800

    Add CRUD for news and exception handling

--- src/main/java/com/collab/g5/demo/news/NewsService.java
+++ src/main/java/com/collab/g5/demo/news/NewsService.java
@@ -4,4 +4,8 @@ import java.util.List;
 
 public interface NewsService {
     List<News> getAllNews();
+    News getNewsById(int id);
+    News addNews(News news);
+    News updateNews(int nID, News news);
+    void deleteNews(int id);
 }
--- src/main/java/com/collab/g5/demo/news/NewsServiceImpl.java
+++ src/main/java/com/collab/g5/demo/news/NewsServiceImpl.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class NewsServiceImpl implements NewsService{
@@ -13,6 +14,35 @@ public class NewsServiceImpl implements NewsService{
     @Override
     public List<News> getAllNews(){
         return newsRepository.findAll();
+    }
+
+    @Override
+    public News getNewsById(int id){
+        return newsRepository.findById(id).map(news -> {
+            return news;
+        }).orElse(null);
+    }
+
+    @Override
+    public News addNews(News news) {
+        Optional<News> newsExists = newsRepository.findById(news.getNID());
+        if(newsExists.isPresent()){
+            throw new IllegalStateException("News " + news.toString() + " exists");
+        }
+        return newsRepository.save(news);
+    }
+
+    @Override
+    public News updateNews(int nID, News freshNews) {
+        News tempNews = newsRepository.findById(nID)
+                .orElseThrow(() -> new IllegalStateException("News with id " + nID + " does not exist"));
+        tempNews.setTitle(freshNews.getTitle());
+        return tempNews;
+    }
+
+    @Override
+    public void deleteNews(int id) {
+        newsRepository.deleteById(id);
     }
 
 }
