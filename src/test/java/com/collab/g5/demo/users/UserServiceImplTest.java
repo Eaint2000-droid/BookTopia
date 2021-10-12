1ed93ae00faa052c039639f871096a13967946c0
charleneteo <charleneteo.2020@sis.smu.edu.sg>
2021-10-12 22:36:07 +0800

    user security

--- src/test/java/com/collab/g5/demo/users/UserServiceImplTest.java
+++ src/test/java/com/collab/g5/demo/users/UserServiceImplTest.java
@@ -1,5 +1,6 @@
 package com.collab.g5.demo.users;
 
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -11,7 +12,6 @@ import static org.mockito.Mockito.when;
 import com.collab.g5.demo.bookings.BookingVetting;
 import com.collab.g5.demo.bookings.Bookings;
 import com.collab.g5.demo.companies.Company;
-import com.collab.g5.demo.exceptions.users.EmailExistsException;
 import com.collab.g5.demo.news.News;
 import com.collab.g5.demo.regulations.RegulationLimit;
 import com.collab.g5.demo.security.PasswordEncoder;
@@ -94,55 +94,7 @@ class UserServiceImplTest {
         user.setBookings(new ArrayList<Bookings>());
         user.setLocked(true);
         user.setUserRole(UserRole.EMPLOYEE);
-        Optional<User> ofResult = Optional.<User>of(user);
-        when(this.userRepository.findByEmail((String) any())).thenReturn(ofResult);
-
-        Company company1 = new Company();
-        company1.setUsers(new ArrayList<User>());
-        company1.setName("Name");
-        company1.setSize(3L);
-        company1.setRegulationLimit(new ArrayList<RegulationLimit>());
-        company1.setCid(1);
-
-        User user1 = new User();
-        user1.setEmail("jane.doe@example.org");
-        user1.setPassword("iloveyou");
-        user1.setCompany(company1);
-        user1.setBookingVetting(new ArrayList<BookingVetting>());
-        user1.setNewsList(new ArrayList<News>());
-        user1.setFname("Fname");
-        user1.setLname("Lname");
-        user1.setEnabled(true);
-        user1.setBookings(new ArrayList<Bookings>());
-        user1.setLocked(true);
-        user1.setUserRole(UserRole.EMPLOYEE);
-        assertThrows(EmailExistsException.class, () -> this.userServiceImpl.addNewUser(user1));
-        verify(this.userRepository).findByEmail((String) any());
-    }
-
-    @Test
-    void testAddNewUser2() {
-        Company company = new Company();
-        company.setUsers(new ArrayList<User>());
-        company.setName("Name");
-        company.setSize(3L);
-        company.setRegulationLimit(new ArrayList<RegulationLimit>());
-        company.setCid(1);
-
-        User user = new User();
-        user.setEmail("jane.doe@example.org");
-        user.setPassword("iloveyou");
-        user.setCompany(company);
-        user.setBookingVetting(new ArrayList<BookingVetting>());
-        user.setNewsList(new ArrayList<News>());
-        user.setFname("Fname");
-        user.setLname("Lname");
-        user.setEnabled(true);
-        user.setBookings(new ArrayList<Bookings>());
-        user.setLocked(true);
-        user.setUserRole(UserRole.EMPLOYEE);
         when(this.userRepository.save((User) any())).thenReturn(user);
-        when(this.userRepository.findByEmail((String) any())).thenReturn(Optional.<User>empty());
 
         Company company1 = new Company();
         company1.setUsers(new ArrayList<User>());
@@ -164,7 +116,6 @@ class UserServiceImplTest {
         user1.setLocked(true);
         user1.setUserRole(UserRole.EMPLOYEE);
         this.userServiceImpl.addNewUser(user1);
-        verify(this.userRepository).findByEmail((String) any());
         verify(this.userRepository).save((User) any());
         assertTrue(this.userServiceImpl.getAllUsers().isEmpty());
     }
@@ -210,8 +161,9 @@ class UserServiceImplTest {
     @Test
     void testGetUserByEmail2() {
         when(this.userRepository.findByEmail((String) any())).thenReturn(Optional.<User>empty());
-        assertThrows(UsernameNotFoundException.class, () -> this.userServiceImpl.getUserByEmail("jane.doe@example.org"));
+        assertNull(this.userServiceImpl.getUserByEmail("jane.doe@example.org"));
         verify(this.userRepository).findByEmail((String) any());
+        assertTrue(this.userServiceImpl.getAllUsers().isEmpty());
     }
 
     @Test
