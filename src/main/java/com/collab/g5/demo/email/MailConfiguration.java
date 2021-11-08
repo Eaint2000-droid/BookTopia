926dcec9e49026ea74deb0cfd1e0675476896c5e
Jing Yuan <jyphee.2020@scis.smu.edu.sg>
2021-11-09 00:14:52 +0800

    Merge conflicts

--- src/main/java/com/collab/g5/demo/email/MailConfiguration.java
+++ src/main/java/com/collab/g5/demo/email/MailConfiguration.java
@@ -11,11 +11,6 @@ import java.util.Properties;
 
 @Configuration
 public class MailConfiguration {
-<<<<<<< HEAD
-    @Autowired
-    private Environment env;
-
-=======
     private Environment env;
 
     @Autowired
@@ -23,7 +18,6 @@ public class MailConfiguration {
         this.env = env;
     }
 
->>>>>>> 59fd609dc73c56dfda25108a2473a568a4c59605
     @Bean
     public JavaMailSender getMailSender() {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
